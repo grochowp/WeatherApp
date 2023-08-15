c3c979252558b398694649bdc195e65a377a7281
grochowp <patgro3@wp.pl>
2023-08-15 17:42:46 +0200

    Added localStorage and delete button

--- script.js
+++ script.js
@@ -2,92 +2,32 @@ class App {
   _parentElement = document.querySelector('.weathers');
   searchBar = document.querySelector('.search__bar');
   searchIcon = document.querySelector('.searchIcon');
+  deleteBtn = document.querySelector('.delete__btn');
+
   #map;
   #mapZoomLevel = 13;
-  #mapEvent;
+  #cities = [];
 
   constructor() {
-    // get user position
-    this._getPosition();
-    this.searchIcon.addEventListener('click', this._findCity.bind(this));
-  }
+    this._getCurrentPosition();
+    console.log(this.#cities);
+    this._getLocalStorage();
 
-  _generateMarkup(data) {
-    console.log(data);
-  }
+    this.searchIcon.addEventListener('click', this._addCityBySearch.bind(this));
 
-  _findCity() {
-    const city = this.searchBar.value;
-    const getCity = async function (city) {
-      try {
-        const response = await fetch(
-          `http://api.weatherapi.com/v1/current.json?key=0d28c2c2e0c840429fb100110231408&q=${city}&aqi=no
-          `
-        );
-        const data = await response.json();
-        return data;
-      } catch (err) {
-        console.log(err);
+    this.searchBar.addEventListener('keydown', e => {
+      if (e.key === 'Enter') {
+        this._addCityBySearch();
       }
-    };
-    getCity(city).then(data => {
-      const curData = data.current;
-      console.log(data);
-      const { lat, lon } = data.location;
-      // const { longitude } = position.coords;
-
-      const coords = [lat, lon];
-      console.log(coords);
-      L.marker(coords)
-        .addTo(this.#map)
-        .bindPopup(
-          L.popup({
-            minWidth: 100,
-            minHeight: 200,
-            autoClose: false,
-            closeOnClick: false,
-          })
-        )
-        .setPopupContent(
-          `Location: ${data.location.name} Temp C: ${curData.temp_c} Wind kph: ${curData.wind_kph}`
-        )
-        .openPopup();
-
-      let image;
-
-      if (curData.temp_c > 25) {
-        image = 'clear';
-      } else if (
-        curData.cloud > 10 &&
-        curData.temp_c > 15 &&
-        curData.temp_c < 25
-      ) {
-        image = 'clouds';
-      } else {
-        image = 'mist';
-      }
-      const markup = `<div id="weather_wrapper">
-      <div class="weatherCard">
-        <div class="currentTemp">
-          <span class="temp">${curData.temp_c}&deg;</span>
-          <span class="location">${data.location.name}</span>
-        </div>
-        <div class="currentWeather">
-          <span class="conditions"
-            ><img class="weather__temp" src="images/${image}.png"
-          /></span>
-          <div class="info">
-            <span class="rain">${curData.humidity}  MM</span>
-            <span class="wind">${curData.wind_kph} MPH</span>
-          </div>
-        </div>
-      </div>
-    </div>`;
-
-      this._parentElement.insertAdjacentHTML('afterbegin', markup);
     });
+    // console.log(this.deleteBtn);
+    this.deleteBtn.addEventListener(
+      'click',
+      this._clearLocalStorage.bind(this)
+    );
   }
-  _getPosition() {
+
+  _getCurrentPosition() {
     if (navigator.geolocation)
       navigator.geolocation.getCurrentPosition(
         this._loadMap.bind(this),
@@ -103,92 +43,165 @@ class App {
 
     const coords = [latitude, longitude];
     this.#map = L.map('map').setView(coords, this.#mapZoomLevel);
-    // console.log(map);
 
     L.tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png', {
       attribution:
         '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors',
     }).addTo(this.#map);
 
-    L.marker(coords)
-      .addTo(this.#map)
-      .bindPopup('Your current location')
-      .openPopup();
+    ///// Display current position
+    // L.marker(coords)
+    //   .addTo(this.#map)
+    //   .bindPopup('Your current location')
+    //   .openPopup();
 
-    this.#map.on('click', this._showWeather.bind(this));
+    this.#map.on('click', this._addCityByClick.bind(this));
+
+    this.#cities.forEach(city => {
+      this._renderWeatherMarker(city);
+    });
   }
 
-  _searchCity(input) {}
+  _renderWeatherMarker(data) {
+    if (data === null) return;
+    const curData = data.current;
+    const locData = data.location;
+    const coords = [locData.lat, locData.lon];
+    L.marker(coords)
+      .addTo(this.#map)
+      .bindPopup(
+        L.popup({
+          minWidth: 100,
+          minHeight: 200,
+          autoClose: false,
+          closeOnClick: false,
+        })
+      )
+      .setPopupContent(
+        `Location: ${locData.name} Temp C: ${curData.temp_c} Wind kph: ${curData.wind_kph}`
+      )
+      .openPopup();
+  }
 
-  _showWeather(mapE) {
-    this.#mapEvent = mapE;
-    const { lat, lng } = mapE.latlng;
-    const coords = [lat, lng];
+  _renderWeather(data) {
+    if (data === null) return '';
+    const curData = data.current;
+    let image;
 
-    const getCity = async function (lat, lng) {
+    if (curData.temp_c > 25) {
+      image = 'clear';
+    } else if (
+      curData.cloud > 10 &&
+      curData.temp_c > 15 &&
+      curData.temp_c < 25
+    ) {
+      image = 'clouds';
+    } else {
+      image = 'mist';
+    }
+    const markup = `<div id="weather_wrapper">
+          <div class="weatherCard">
+          <div class="currentTemp">
+          <span class="temp">${curData.temp_c}&deg;</span>
+          <span class="location">${data.location.name}</span>
+          </div>
+          <div class="currentWeather">
+          <span class="conditions"
+          ><img class="weather__temp" src="images/${image}.png"
+          /></span>
+          <div class="info">
+          <span class="rain">${curData.humidity}  MM</span>
+          <span class="wind">${curData.wind_kph} MPH</span>
+          </div>
+          </div>
+          </div>
+          </div>`;
+    this._parentElement.insertAdjacentHTML('afterbegin', markup);
+  }
+
+  // Funciton to find city in API by search bar
+  _addCityBySearch() {
+    const city = this.searchBar.value;
+
+    if (!city) return;
+
+    const findCityFromAPI = async function (city) {
       try {
         const response = await fetch(
-          `http://api.weatherapi.com/v1/current.json?key=0d28c2c2e0c840429fb100110231408&q=${lat},${lng}`
+          `http://api.weatherapi.com/v1/current.json?key=0d28c2c2e0c840429fb100110231408&q=${city}&aqi=no
+          `
         );
         const data = await response.json();
-        console.log(data);
         return data;
       } catch (err) {
         console.log(err);
       }
     };
 
-    getCity(lat, lng).then(data => {
-      const curData = data.current;
+    // Display markup to city found by search or click on map
+    findCityFromAPI(city).then(data => {
+      const isDuplicate = this.#cities.some(
+        cityObj => cityObj.location.name === data.location.name
+      );
 
-      L.marker(coords)
-        .addTo(this.#map)
-        .bindPopup(
-          L.popup({
-            minWidth: 100,
-            minHeight: 200,
-            autoClose: false,
-            closeOnClick: false,
-          })
-        )
-        .setPopupContent(
-          `Location: ${data.location.name} Temp C: ${curData.temp_c} Wind kph: ${curData.wind_kph}`
-        )
-        .openPopup();
+      if (isDuplicate || this.#cities.length === 4) return;
 
-      let image;
-
-      if (curData.temp_c > 25) {
-        image = 'clear';
-      } else if (
-        curData.cloud > 10 &&
-        curData.temp_c > 15 &&
-        curData.temp_c < 25
-      ) {
-        image = 'clouds';
-      } else {
-        image = 'mist';
-      }
-      const markup = `<div id="weather_wrapper">
-      <div class="weatherCard">
-        <div class="currentTemp">
-          <span class="temp">${curData.temp_c}&deg;</span>
-          <span class="location">${data.location.name}</span>
-        </div>
-        <div class="currentWeather">
-          <span class="conditions"
-            ><img class="weather__temp" src="images/${image}.png"
-          /></span>
-          <div class="info">
-            <span class="rain">${curData.humidity}  MM</span>
-            <span class="wind">${curData.wind_kph} MPH</span>
-          </div>
-        </div>
-      </div>
-    </div>`;
-
-      this._parentElement.insertAdjacentHTML('afterbegin', markup);
+      this._renderWeather(data);
+      this._renderWeatherMarker(data);
+      this.#cities.push(data);
+      this._setLocalStorage();
+      this.searchBar.value = '';
     });
+  }
+
+  _addCityByClick(mapE) {
+    const { lat, lng } = mapE.latlng;
+
+    // || city === this.#cities.location.name.includes(city)
+    const findCityFromAPI = async function (lat, lng) {
+      try {
+        const response = await fetch(
+          `http://api.weatherapi.com/v1/current.json?key=0d28c2c2e0c840429fb100110231408&q=${lat},${lng}`
+        );
+        const data = await response.json();
+        return data;
+      } catch (err) {
+        console.log(err);
+      }
+    };
+
+    findCityFromAPI(lat, lng).then(data => {
+      const isDuplicate = this.#cities.some(
+        cityObj => cityObj.location.name === data.location.name
+      );
+
+      if (isDuplicate || this.#cities.length === 4) return;
+
+      this._renderWeather(data);
+      this._renderWeatherMarker(data);
+      this.#cities.push(data);
+      this._setLocalStorage();
+    });
+  }
+
+  //////// TO DO
+  _setLocalStorage() {
+    localStorage.setItem('weather', JSON.stringify(this.#cities));
+  }
+
+  _getLocalStorage() {
+    const data = JSON.parse(localStorage.getItem('weather'));
+    if (!data) return;
+
+    this.#cities = data;
+    this.#cities.forEach(city => {
+      this._renderWeather(city);
+    });
+  }
+
+  _clearLocalStorage() {
+    localStorage.removeItem('weather');
+    location.reload();
   }
 }
 
