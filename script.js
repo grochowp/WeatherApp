20d2a2f662c401b4958eb2a722d1b870ce483d01
grochowp <patgro3@wp.pl>
2023-08-15 11:01:50 +0200

    Added searching by city name

--- script.js
+++ script.js
@@ -1,4 +1,7 @@
 class App {
+  _parentElement = document.querySelector('.weathers');
+  searchBar = document.querySelector('.search__bar');
+  searchIcon = document.querySelector('.searchIcon');
   #map;
   #mapZoomLevel = 13;
   #mapEvent;
@@ -6,8 +9,84 @@ class App {
   constructor() {
     // get user position
     this._getPosition();
+    this.searchIcon.addEventListener('click', this._findCity.bind(this));
   }
 
+  _generateMarkup(data) {
+    console.log(data);
+  }
+
+  _findCity() {
+    const city = this.searchBar.value;
+    const getCity = async function (city) {
+      try {
+        const response = await fetch(
+          `http://api.weatherapi.com/v1/current.json?key=0d28c2c2e0c840429fb100110231408&q=${city}&aqi=no
+          `
+        );
+        const data = await response.json();
+        return data;
+      } catch (err) {
+        console.log(err);
+      }
+    };
+    getCity(city).then(data => {
+      const curData = data.current;
+      console.log(data);
+      const { lat, lon } = data.location;
+      // const { longitude } = position.coords;
+
+      const coords = [lat, lon];
+      console.log(coords);
+      L.marker(coords)
+        .addTo(this.#map)
+        .bindPopup(
+          L.popup({
+            minWidth: 100,
+            minHeight: 200,
+            autoClose: false,
+            closeOnClick: false,
+          })
+        )
+        .setPopupContent(
+          `Location: ${data.location.name} Temp C: ${curData.temp_c} Wind kph: ${curData.wind_kph}`
+        )
+        .openPopup();
+
+      let image;
+
+      if (curData.temp_c > 25) {
+        image = 'clear';
+      } else if (
+        curData.cloud > 10 &&
+        curData.temp_c > 15 &&
+        curData.temp_c < 25
+      ) {
+        image = 'clouds';
+      } else {
+        image = 'mist';
+      }
+      const markup = `<div id="weather_wrapper">
+      <div class="weatherCard">
+        <div class="currentTemp">
+          <span class="temp">${curData.temp_c}&deg;</span>
+          <span class="location">${data.location.name}</span>
+        </div>
+        <div class="currentWeather">
+          <span class="conditions"
+            ><img class="weather__temp" src="images/${image}.png"
+          /></span>
+          <div class="info">
+            <span class="rain">${curData.humidity}  MM</span>
+            <span class="wind">${curData.wind_kph} MPH</span>
+          </div>
+        </div>
+      </div>
+    </div>`;
+
+      this._parentElement.insertAdjacentHTML('afterbegin', markup);
+    });
+  }
   _getPosition() {
     if (navigator.geolocation)
       navigator.geolocation.getCurrentPosition(
@@ -39,6 +118,8 @@ class App {
     this.#map.on('click', this._showWeather.bind(this));
   }
 
+  _searchCity(input) {}
+
   _showWeather(mapE) {
     this.#mapEvent = mapE;
     const { lat, lng } = mapE.latlng;
@@ -58,6 +139,8 @@ class App {
     };
 
     getCity(lat, lng).then(data => {
+      const curData = data.current;
+
       L.marker(coords)
         .addTo(this.#map)
         .bindPopup(
@@ -69,12 +152,43 @@ class App {
           })
         )
         .setPopupContent(
-          `Location: ${data.location.name} Temp C: ${data.current.temp_c} Wind kph: ${data.current.wind_kph}`
+          `Location: ${data.location.name} Temp C: ${curData.temp_c} Wind kph: ${curData.wind_kph}`
         )
         .openPopup();
-      console.log();
+
+      let image;
+
+      if (curData.temp_c > 25) {
+        image = 'clear';
+      } else if (
+        curData.cloud > 10 &&
+        curData.temp_c > 15 &&
+        curData.temp_c < 25
+      ) {
+        image = 'clouds';
+      } else {
+        image = 'mist';
+      }
+      const markup = `<div id="weather_wrapper">
+      <div class="weatherCard">
+        <div class="currentTemp">
+          <span class="temp">${curData.temp_c}&deg;</span>
+          <span class="location">${data.location.name}</span>
+        </div>
+        <div class="currentWeather">
+          <span class="conditions"
+            ><img class="weather__temp" src="images/${image}.png"
+          /></span>
+          <div class="info">
+            <span class="rain">${curData.humidity}  MM</span>
+            <span class="wind">${curData.wind_kph} MPH</span>
+          </div>
+        </div>
+      </div>
+    </div>`;
+
+      this._parentElement.insertAdjacentHTML('afterbegin', markup);
     });
-    // console.log(result);
   }
 }
 
